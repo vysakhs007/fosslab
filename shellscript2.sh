fb064dcefd3d9191a0d6a06f5c6ff20c841ee126
vysakhs007 <vysakhsantosh007@gmail.com>
2018-02-20 20:04:33 +0530

    Third commit

---
+++ shellscript2.sh
@@ -0,0 +1,8 @@
+echo "The logged in user is :$(echo $USER)"
+echo "The current used shell is :$(echo $SHELL)"
+echo "The current working directory is :$(echo $HOME)"
+echo "The current operating systrem type is :$(echo $OSTYPE)"
+echo "The current path setting is :$(echo $PWD)"
+echo "The current working directory is :$(echo $PWD)"
+echo "The number of users currently logged in are  :"
+who --count
